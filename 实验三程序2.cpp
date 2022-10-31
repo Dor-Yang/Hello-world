8efb5cdf8752948621da2695c20c5af5d1434a7e
Dor-Yang <2188803191@qq.com>
2022-10-31 19:54:40 +0800

    Create 实验三程序2.cpp

---
+++ 实验三程序2.cpp
@@ -0,0 +1,12 @@
+﻿#include"stdio.h"
+int main()
+{
+	double x,y;
+	printf("请输入x的值：");
+	scanf("%f",&x);
+	if(x<0&&x!=-3) y=x*x+x-6;
+	else if(x>=0&&x<10&&x!=2&&x!=3) y=x*x-5*x+6;
+	else y=x*x-x-1;
+	printf("x=%f,y=%f\n",x,y);
+	return 0;
+}
