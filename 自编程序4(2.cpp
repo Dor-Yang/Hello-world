f0183022da7e0afcb224246a77b0c16efd7098af
Dor-Yang <2188803191@qq.com>
2022-10-31 22:59:55 +0800

    Create 自编程序4(2.cpp

---
+++ 自编程序4(2.cpp
@@ -0,0 +1,12 @@
+﻿#include"stdio.h"
+int main()
+{
+	int a,b,c,d,e,max,min,m1,m2,m3,n1,n2,n3; 
+	printf("请输入5个整数：");
+	scanf("%d,%d,%d,%d,%d",&a,&b,&c,&d,&e);
+	max=a>b?a:b>c?b:c>d?c:d>e?d:e;
+	printf("5个整数中最大值为:%d\n",max);
+	min=a<b?a:b<c?b:c<d?c:d<e?d:e;
+	printf("5个整数中最小值为:%d\n",min);
+    return 0;
+} 
