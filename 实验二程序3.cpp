8b7155d0895c56b06c495751c31436a56b62640a
Dor-Yang <2188803191@qq.com>
2022-11-01 13:34:54 +0800

    Create 实验二程序3.cpp

---
+++ 实验二程序3.cpp
@@ -0,0 +1,15 @@
+﻿#include"stdio.h"
+int main()
+{
+	float rate,capital,deposit=0;
+	int n;
+	printf("Please input the rate:");
+	scanf("%f",&rate);
+	printf("Please input the n:");
+	scanf("%d",&n);
+	printf("Please input the capital:");
+	scanf("%f",&capital);
+	deposit=capital+capital*n*rate/100;
+	printf("The deposit is %f\n",deposit);
+	return 0;
+}
